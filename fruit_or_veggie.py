917e34d1ada1f2c553190a4b39d82b2c0b89aca2
Stanislav Yanakiev <stanislav.yanakievv@gmail.com>
2022-08-18 13:54:29 +0300

    Add files via upload

---
+++ fruit_or_veggie.py
@@ -0,0 +1,10 @@
+fruit_or_vegie = input()
+
+
+if fruit_or_vegie in 'banana apple kiwi cherry lemon grapes':
+    print('fruit')
+
+elif fruit_or_vegie in 'tomato cucumber pepper carrot':
+    print('vegetable')
+else:
+    print('unknown')
