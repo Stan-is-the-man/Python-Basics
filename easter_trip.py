72b1968f869f78aa2cff2e78e835d35393784b4a
Stanislav Yanakiev <stanislav.yanakievv@gmail.com>
2022-09-03 23:37:51 +0300

    Add files via upload

---
+++ easter_trip.py
@@ -0,0 +1,27 @@
+destination = input()
+period = input()
+number_of_nights = int(input())
+
+if destination == "France":
+    if period == "21-23":
+        price = number_of_nights * 30
+    elif period == "24-27":
+        price = number_of_nights * 35
+    elif period == "28-31":
+        price = number_of_nights * 40
+elif destination == "Italy":
+    if period == "21-23":
+        price = number_of_nights * 28
+    elif period == "24-27":
+        price = number_of_nights * 32
+    elif period == "28-31":
+        price = number_of_nights * 39
+else:
+    if period == "21-23":
+        price = number_of_nights * 32
+    elif period == "24-27":
+        price = number_of_nights * 37
+    elif period == "28-31":
+        price = number_of_nights * 43
+
+print(f'Easter trip to {destination} : {price:.2f} leva.')
