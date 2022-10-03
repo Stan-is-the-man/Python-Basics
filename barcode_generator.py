f6ca5c31072cb7a72f7ee46cf4e25baaeaa1a7ae
Stanislav Yanakiev <stanislav.yanakievv@gmail.com>
2022-10-03 10:05:19 +0300

    Update barcode_generator.py

--- barcode_generator.py
+++ barcode_generator.py
@@ -1,5 +1,6 @@
 start_number = input()
 end_number = input()
+
 n_1 = 0
 n_2 = 0
 n_3 = 0
@@ -8,6 +9,7 @@ e_1 = 0
 e_2 = 0
 e_3 = 0
 e_4 = 0
+
 for x, number in enumerate(start_number):
     if x == 0:
         n_1 = int(number)
@@ -17,6 +19,7 @@ for x, number in enumerate(start_number):
         n_3 = int(number)
     else:
         n_4 = int(number)
+        
 for y, number_end in enumerate(end_number):
     if y == 0:
         e_1 = int(number_end)
@@ -26,6 +29,7 @@ for y, number_end in enumerate(end_number):
         e_3 = int(number_end)
     else:
         e_4 = int(number_end)
+        
 for a in range(n_1, e_1 + 1):
     for b in range(n_2, e_2 + 1):
         for c in range(n_3, e_3 + 1):
