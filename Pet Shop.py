f3ff7a8f9dfbf3ab715ce75bd9ccfe049553111b
Stanislav Yanakiev <stanislav.yanakievv@gmail.com>
2022-10-13 09:07:58 +0300

    Add files via upload

---
+++ Pet Shop.py
@@ -0,0 +1,7 @@
+quantity_dog_food = int(input())
+quantity_cat_food = int(input())
+total_dog_food = quantity_dog_food * 2.5
+total_cat_food = quantity_cat_food * 4
+
+total_sum = total_dog_food + total_cat_food
+print(f"{total_sum} lv.")
