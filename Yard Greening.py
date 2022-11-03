83ec9aefce3f2ad4300a358be57813cdb1fc0151
Stanislav Yanakiev <stanislav.yanakievv@gmail.com>
2022-11-03 12:38:14 +0200

    Add files via upload

---
+++ Yard Greening.py
@@ -0,0 +1,7 @@
+sq_m = float(input())
+total = sq_m * 7.61
+discount = total * 0.18
+total_with_discount = total - discount
+
+print(f"The final price is: {total_with_discount} lv.")
+print(f"The discount is: {discount} lv.")
