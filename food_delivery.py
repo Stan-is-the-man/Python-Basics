ddeeb4f4275a409d288b49368fcf9d7eb8fb9022
Stanislav Yanakiev <stanislav.yanakievv@gmail.com>
2022-12-05 20:51:43 +0200

    Update food_delivery.py

--- food_delivery.py
+++ food_delivery.py
@@ -5,7 +5,6 @@ qt_veggie = int(input())
 price_chicken = qt_chicken * 10.35
 price_fish = qt_fish * 12.4
 price_veggie = qt_veggie * 8.15
-
 sum_of_all_dishes = price_veggie + price_fish + price_chicken
 price_desert = 0.2 * sum_of_all_dishes
 
