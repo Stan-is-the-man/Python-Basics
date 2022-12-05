c0f2b964b4383442da4063444e969c0048413350
Stanislav Yanakiev <stanislav.yanakievv@gmail.com>
2022-12-05 20:51:28 +0200

    Update food_delivery.py

--- food_delivery.py
+++ food_delivery.py
@@ -9,7 +9,6 @@ price_veggie = qt_veggie * 8.15
 sum_of_all_dishes = price_veggie + price_fish + price_chicken
 price_desert = 0.2 * sum_of_all_dishes
 
-
 total_price = sum_of_all_dishes + price_desert + 2.5
 
 
